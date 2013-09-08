97beeed99b59bab0f3117bfde893972d2c802f04
Felix Hammerl <felix@whiteout.io>
2013-09-08 22:54:46 +0200

    Port to AMD module
    Use buffer-browserify as dependency
    
    Use forked version of encoding

--- test/mimelib.js
+++ test/mimelib.js
@@ -81,13 +81,13 @@ exports["Quoted printable"] = {
 
     "Surrogate pair": function(test){
         // 💩 pile of poo
-        test.equal("=F0=9F=92=A9", mimelib.encodeQuotedPrintable('\ud83d\udca9'))
-        test.equal("\ud83d\udca9", mimelib.decodeQuotedPrintable('=F0=9F=92=A9'))
+        test.equal("=F0=9F=92=A9", mimelib.encodeQuotedPrintable('\ud83d\udca9'));
+        test.equal("\ud83d\udca9", mimelib.decodeQuotedPrintable('=F0=9F=92=A9'));
         test.done();
     }
-}
+};
 
-exports["Base64"] = {
+exports.Base64 = {
     "Convert string": function(test){
         test.equal("VGVyZSDVxNbcIQ==", mimelib.encodeBase64("Tere ÕÄÖÜ!", "Latin_1"));
         test.done();
@@ -97,7 +97,7 @@ exports["Base64"] = {
         test.equal("Tere ÕÄÖÜ!", mimelib.decodeBase64("VGVyZSDVxNbcIQ==", "Latin_1"));
         test.done();
     }
-}
+};
 
 exports["Mime Words"] = {
     "Encode Mime Word QP": function(test){
@@ -111,7 +111,7 @@ exports["Mime Words"] = {
     },
 
     "Parse Mime Words": function(test){
-        test.equal("Jõge-vaŽ zz Jõge-vaŽJõge-vaŽJõge-vaŽ", mimelib.parseMimeWords("=?ISO-8859-13?Q?J=F5ge-va=DE?= zz =?ISO-8859-13?Q?J=F5ge-va=DE?= =?ISO-8859-13?Q?J=F5ge-va=DE?= =?ISO-8859-13?Q?J=F5ge-va=DE?="))
+        test.equal("Jõge-vaŽ zz Jõge-vaŽJõge-vaŽJõge-vaŽ", mimelib.parseMimeWords("=?ISO-8859-13?Q?J=F5ge-va=DE?= zz =?ISO-8859-13?Q?J=F5ge-va=DE?= =?ISO-8859-13?Q?J=F5ge-va=DE?= =?ISO-8859-13?Q?J=F5ge-va=DE?="));
         test.equal("Sssś Lałalalala", mimelib.parseMimeWords("=?UTF-8?B?U3NzxZsgTGHFgmFsYQ==?= =?UTF-8?B?bGFsYQ==?="));
         test.done();
     },
@@ -121,7 +121,7 @@ exports["Mime Words"] = {
             outputStr = "=?ISO-8859-1?Q?J=F5geva_J=F5gev?= =?ISO-8859-1?Q?a_J=F5geva?= mugeva =?ISO-8859-1?Q?J=F5geva_J=F5gev?= =?ISO-8859-1?Q?a_J=F5geva_J=F5g?= =?ISO-8859-1?Q?eva_J=F5geva?=",
             encoded = mimelib.encodeMimeWords(inputStr, "Q", 16, "ISO-8859-1");
 
-        test.equal(outputStr, encoded)
+        test.equal(outputStr, encoded);
         test.equal(inputStr, mimelib.parseMimeWords(encoded));
         test.done();
     },
@@ -131,7 +131,7 @@ exports["Mime Words"] = {
             outputStr = "=?ISO-8859-1?B?SvVnZXZhIEr1Z2V2?= =?ISO-8859-1?B?YSBK9WdldmE=?= mugeva =?ISO-8859-1?B?SvVnZXZhIEr1Z2V2?= =?ISO-8859-1?B?YSBK9WdldmEgSvVn?= =?ISO-8859-1?B?ZXZhIEr1Z2V2YQ==?=",
             encoded = mimelib.encodeMimeWords(inputStr,"B", 16, "ISO-8859-1");
 
-        test.equal(outputStr, encoded)
+        test.equal(outputStr, encoded);
         test.equal(inputStr, mimelib.parseMimeWords(encoded));
         test.done();
     },
@@ -149,8 +149,8 @@ exports["Mime Words"] = {
         test.equal(mimelib.parseMimeWords(output2), input2);
 
         test.done();
-    } 
-}
+    }
+};
 
 exports["Fold long line"] = function(test){
     var inputStr = "Subject: Testin command line kirja õkva kakva mõni tõnis kõllas põllas tõllas rõllas jušla kušla tušla musla",
@@ -161,4 +161,4 @@ exports["Fold long line"] = function(test){
 
     test.equal(outputStr, mimelib.foldLine(mimelib.encodeMimeWords(inputStr, "Q", 52), 76, false, false, 52));
     test.done();
-}
+};
